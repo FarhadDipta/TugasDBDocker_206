cf5f86ab594a9689fe1fb920eae14fec4a649c47
FarhadDipta <160616287+FarhadDipta@users.noreply.github.com>
2026-04-22 10:49:56 +0700

    Menambahkan GetMapping by id pada UserController.java

--- src/main/java/com/praktikumDB/deploy/controller/UserController.java
+++ src/main/java/com/praktikumDB/deploy/controller/UserController.java
@@ -26,4 +26,9 @@ public class UserController {
     public List<User> getAllUsers(){
         return userService.getAllUsers();
     }
+
+    @GetMapping("/{id}")
+    public User getUserById(@PathVariable String id){
+        return userService.getUserById(id);
+    }
 }

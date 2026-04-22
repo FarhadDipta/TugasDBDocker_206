0158917ec234c373cf5f139be079ac15a8964e2e
FarhadDipta <160616287+FarhadDipta@users.noreply.github.com>
2026-04-22 10:48:43 +0700

    Menambahkan PostMapping pada UserController.java

---
+++ src/main/java/com/praktikumDB/deploy/controller/UserController.java
@@ -0,0 +1,25 @@
+package com.praktikumDB.deploy.controller;
+
+import com.praktikumDB.deploy.model.User;
+import com.praktikumDB.deploy.service.UserService;
+import org.springframework.stereotype.Repository;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@Repository
+@RequestMapping ("/api/users")
+public class UserController {
+    private final UserService userService;
+
+    public UserController(UserService userService) {
+        this.userService = userService;
+    }
+
+    @PostMapping
+    public String createUser(@RequestBody User request){
+        userService.addUser(request);
+        return "user created succesfully";
+    }
+
+}
